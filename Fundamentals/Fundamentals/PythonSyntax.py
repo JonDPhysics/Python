46a236d6d2582a7543761d5437baf8a86104f8a7
Jonathan Smith <jonathan.smith722@gmail.com>
2021-10-23 15:04:22 -0700

    Updated 10/23/21

---
+++ Fundamentals/Fundamentals/PythonSyntax.py
@@ -0,0 +1,14 @@
+x = 10
+if x > 50:
+    print("bigger than 50")
+else:
+    print("smaller than 50")
+
+class EmptyClass:
+    pass
+
+my_string = ""
+
+for val in my_string:
+    pass
+
---
+++ Fundamentals/Fundamentals/tuples.py
@@ -0,0 +1,36 @@
+tuple_data = ('physics', 'chemistry', 1997, 2000)
+tuple_num = (1, 2, 3, 4, 5 )
+tuple_letters = "a", "b", "c", "d"
+
+dog = ("Canis Familiaris", "dog", "carnivore", 12)
+
+print(dog[2])
+#result is: carnivore
+"""
+dog[0] = "X"
+TypeError: 'tuple' object does not support item assignment
+"""
+
+dog = dog + ("domestic",)
+#result is...
+#("Canis Familiaris", "Dog", "carnivore", 12, "domestic")
+
+dog = dog[:3] + ("man's best friend",) + dog[4:]
+#result is...
+#("Canis Familiaris", "Dog", "carnivore", "man's best friend", "domestic")
+
+x = (1,5,6,9,2)
+print(len(x))
+# output:
+# 5
+
+def get_circle_area(r):
+    #Return (circumference, area) of a circle of radius r
+    c = 2 * math.pi * r
+    a = math.pi * r * r
+    return (c, a)
+
+import language
+print(language.translate(dog))
+# output would look something like: ("dog", "chien", "perro")
+
