7ab54728daa6e4c838032c1b59ba625e2e22fee5
Jonathan Smith <jonathan.smith722@gmail.com>
2021-10-23 19:02:04 -0700

    Updated 10/23/21

--- Fundamentals/Fundamentals/functions_basic_i/functions_basic_i.py
+++ Fundamentals/Fundamentals/functions_basic_i/functions_basic_i.py
@@ -9,9 +9,6 @@ def number_of_military_branches():
     return 5
 print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
 
-
-
-
 #3
 def number_of_books_on_hold():
     return 5
