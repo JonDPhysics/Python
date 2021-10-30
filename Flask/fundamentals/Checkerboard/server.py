5535e030719fb05adf6e5d329113b54cb21a68d1
Jonathan Smith <jonathan.smith722@gmail.com>
2021-10-29 18:54:07 -0700

    Updated 10/29/21

--- Flask/fundamentals/Checkerboard/server.py
+++ Flask/fundamentals/Checkerboard/server.py
@@ -3,8 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def board():
-    return render_template('index.html')
+@app.route('/<int:x>')
+@app.route('/<int:x>/<int:y>')
+# @app.route('/<int:x/<int:y>/<color1>')
+# @app.route('/<int:x/<int:y>/<color1>/<color2>')
+def board(x = 8, y = 8): # color1 = "black", color2 = "red"
+    y /= 2
+    return render_template('index.html', roe = x, col = int(y)) # color1 = color1, color2 = color2
 
 if __name__=='__main__':
     app.run(debug=True)
