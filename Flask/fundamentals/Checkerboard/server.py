de26a95c8412b9bad345db3e2ecee6e99453a3d0
Jonathan Smith <jonathan.smith722@gmail.com>
2021-10-30 11:09:32 -0700

    Updated 10/30/21

--- Flask/fundamentals/Checkerboard/server.py
+++ Flask/fundamentals/Checkerboard/server.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # @app.route('/<int:x/<int:y>/<color1>')
 # @app.route('/<int:x/<int:y>/<color1>/<color2>')
 def board(x = 8, y = 8): # color1 = "black", color2 = "red"
-    y /= 2
+    y /= 2 
     return render_template('index.html', roe = x, col = int(y)) # color1 = color1, color2 = color2
 
 if __name__=='__main__':
--- Flask/fundamentals/HTML Table/server.py
+++ Flask/fundamentals/HTML Table/server.py
@@ -1,18 +1,17 @@
-from logging import debug
 from flask import Flask, render_template
 app = Flask(__name__)
 
-users = [
-{'first_name' : 'Michael', 'last_name' : 'Choi'},
-{'first_name' : 'John', 'last_name' : 'Supsupin'},
-{'first_name' : 'Mark', 'last_name' : 'Guillen'},
-{'first_name' : 'KB', 'last_name' : 'Tonel'}
-]
 
 
 @app.route('/')
 def data():
-    return render_template('index.html')
+    users = [
+        {'first_name' : 'Michael', 'last_name' : 'Choi', 'full_name': 'Micheal Choi'},
+        {'first_name' : 'John', 'last_name' : 'Supsupin', 'full_name': 'John Supsupin'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen', 'full_name': 'Mark Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel', 'full_name': 'KB Tonel'}
+]
+    return render_template('index.html', users = users)
 
 if __name__=="__main__":
     app.run(debug=True)
