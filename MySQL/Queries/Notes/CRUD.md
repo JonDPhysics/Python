f8061b23f02ad4516b7837e576b58b9cd7e701ff
Jonathan Smith <jonathan.smith722@gmail.com>
2021-11-03 16:38:46 -0700

    Updated 11/03/21

--- MySQL/Queries/Notes/CRUD.md
+++ MySQL/Queries/Notes/CRUD.md
@@ -1,8 +1,9 @@
 CRUD FOR MYSQL SYNTAX:
 -Create -INSERT INTO table (table_columns1, table_columns2, etc) VALUES (value1, value2, etc)
 
--Retreive/Read  -SELECT column (*(all), column1, column2,...., column_) FROM table
-                -SELECT *, concat_ws(column,other_column) AS name_of_column FROM shema.table;
+-Retreive/Read  
+        -SELECT column (*(all), column1, column2,...., column_) FROM table
+        -SELECT *, concat_ws(column,other_column) AS name_of_column FROM shema.table;
 
 -Update -UPDATE table SET column = value WHERE id = x
 
--- flaskMySQL/dbConnection/first_flask/server.py
+++ flaskMySQL/dbConnection/first_flask/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request,redirect
 # import the class from friend.py
 from friend import Friend
 app = Flask(__name__)
@@ -6,6 +6,16 @@ app = Flask(__name__)
 def index():
     # call the get all classmethod to get all friends
     return render_template("index.html", friends = Friend.get_all())
+
+@app.route("/create_friend", methods = ["POST"])
+def create_friend():
+    data = {
+        "fname": request.form["fname"],
+        "lname": request.form["lname"],
+        "occ": request.form["occ"]
+    }
+    Friend.save(data)
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
