27152e6919469cc9214ffa720b74e29e6423364d
Jonathan Smith <jonathan.smith722@gmail.com>
2021-11-03 14:35:51 -0700

    Updated 11/03/21

--- flaskMySQL/dbConnection/first_flask/server.py
+++ flaskMySQL/dbConnection/first_flask/server.py
@@ -5,9 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     # call the get all classmethod to get all friends
-    friends = Friend.get_all()
-    print(friends)
-    return render_template("index.html")
+    return render_template("index.html", friends = Friend.get_all())
 
 if __name__ == "__main__":
     app.run(debug=True)
