e7cf5296b585cc9f5806db202c15537afae3ad4b
Jonathan Smith <jonathan.smith722@gmail.com>
2021-11-04 14:36:49 -0700

    Updated 11/04/21

--- flaskMySQL/Boilerplate Code/flask_app/controllers/examples.py
+++ flaskMySQL/Boilerplate Code/flask_app/controllers/examples.py
@@ -1,16 +1,17 @@
+from typing import ParamSpecArgs
 from flask.globals import request
 from flask_app import app
 from flask import render_template, redirect, request, session
-from example import Example # name change needed
+from flask_app.models.example import Example # name change needed
 
 @app.route("/")
 def start():
-    return render_template("index.html")
+    return render_template("index.html", examples = Example.get_all())
 
 @app.route("/add", methods=['POST'])
 def add():
-    return redirect("/")
+    pass
 
 @app.route ("/edit", methods=['POST'])
 def edit():
-    return redirect("/")
+    pass
--- flaskMySQL/Boilerplate Code/flask_app/models/example.py
+++ flaskMySQL/Boilerplate Code/flask_app/models/example.py
@@ -3,9 +3,7 @@ from flask_app.config.mysqlconnection import connectToMySQL
 class Example: # name change needed
     def __init__( self , data ):
         self.id = data['id']
-        self.blank1 = data['blank1'] #name change needed
-        self.blank2 = data['blank2'] #name change needed
-        self.blank3 = data['blank3'] #name change needed
+        #Add needed columns
         self.created_at = data['created_at'] #name change needed
         self.updated_at = data['updated_at'] #name change needed
 
