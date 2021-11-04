17122f54c5c7d2df07cb6f473dab24de488b491f
Jonathan Smith <jonathan.smith722@gmail.com>
2021-11-04 10:38:54 -0700

    Updated 11/04/21

--- flaskMySQL/Users CR/users_cr/server.py
+++ flaskMySQL/Users CR/users_cr/server.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect
+# import the class from friend.py
+from user import User
 app = Flask(__name__)
-app.secret_key ='scooby-doo where are you???'
+@app.route("/users")
+def index():
+    # call the get all classmethod to get all friends
+    return render_template("index.html", users = User.get_all())
 
-@app.route('/users')
-def users():
-    return render_template("index.html")
+@app.route("/users/new")
+def new_user():
 
+    render_template("add.html")
 
-if __name__=='__main__':
+@app.route("/create_user", methods = ["POST"])
+def create_user():
+    data = {
+        "fullname": request.form["fname", "lname"],
+        "email": request.form["email"]
+    }
+    User.save(data)
+    return redirect("/users")
+
+@app.route("/add_user", methods = ["POST"])
+def add_users():
+
+    return redirect("/users/new")
+
+if __name__ == "__main__":
     app.run(debug=True)
