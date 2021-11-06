40a1bffa6b0b09053aa20da057cc34fb7b2db6ee
Jonathan Smith <jonathan.smith722@gmail.com>
2021-11-05 18:51:59 -0700

    Updated 11/05/21

--- flaskMySQL/CRUD/Users CRUD/server.py
+++ flaskMySQL/CRUD/Users CRUD/server.py
@@ -9,6 +9,25 @@ def index():
     # call the get all classmethod to get all friends
     return render_template("index.html", users = User.get_all())
 
+@app.route("/show")
+def show_user():
+    return render_template("show.html", users = User.get_one())
+
+@app.route("/edit", methods=["POST"])
+def edit_user():
+    data = {
+        "fname": request.form["fname"], 
+        "lname": request.form["lname"],
+        "email": request.form["email"],
+    }
+    User.change(data)
+    return redirect("/users",)
+
+@app.route("/delete")
+def delete_user():
+    User.delete(data):
+    return render_template("index.html")
+
 @app.route("/new")
 def new_user():
     return render_template("add.html")
